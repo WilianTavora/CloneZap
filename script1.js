752919b26525729601ff59ccef2c03007aee53f2
Wilian Tavora <angeltavora@gmail.com>
2023-07-21 13:32:34 -0300

    update

---
+++ script1.js
@@ -0,0 +1,11 @@
+const list = document.querySelectorAll(".list")
+console.log("ok");
+        
+        function activelink(){
+            list.forEach((item)=>
+            item.classList.remove("active")
+            )
+            this.classList.add('active')
+        }
+        list.forEach((item)=>
+            item.addEventListener("click", activelink))
